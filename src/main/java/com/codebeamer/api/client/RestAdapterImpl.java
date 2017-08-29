987d08526d1fd9224d76b864c4e5db9382a83858
Martin Gansler <martin.gansler@intland.com>
2017-08-29 08:31:13 +0200

    add post requests

--- src/main/java/com/codebeamer/api/client/RestAdapterImpl.java
+++ src/main/java/com/codebeamer/api/client/RestAdapterImpl.java
@@ -15,7 +15,10 @@ import org.apache.http.auth.InvalidCredentialsException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpRequestBase;
 import org.apache.http.conn.ConnectTimeoutException;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.message.BasicHeader;
@@ -74,21 +77,36 @@ public class RestAdapterImpl implements RestAdapter {
     }
 
     private String executeGet(String uri) throws ConnectTimeoutException, InvalidCredentialsException {
-        logger.info(uri);
         HttpGet get = new HttpGet(uri);
         get.setConfig(requestConfig);
 
+        return executeRest(get);
+    }
+
+    private String executePost(String uri, String content) throws ConnectTimeoutException, InvalidCredentialsException {
+        StringEntity entity = new StringEntity(content, Charsets.UTF_8);
+        entity.setContentType("application/json");
+
+        HttpPost post = new HttpPost(uri);
+        post.setConfig(requestConfig);
+        post.setEntity(entity);
+
+        return executeRest(post);
+    }
+
+    private String executeRest(HttpRequestBase request) throws ConnectTimeoutException, InvalidCredentialsException {
+        logger.debug(String.format("%s-request to %s", request.getConfig(), request.getURI()));
         try {
-            HttpResponse response = client.execute(get);
+            HttpResponse response = client.execute(request);
             if (response.getStatusLine().getStatusCode() == HttpStatus.SC_UNAUTHORIZED) {
                 throw new InvalidCredentialsException("incorrect credentials");
             }
             return new BasicResponseHandler().handleResponse(response);
-        } catch (IOException ex) {
-            logger.debug(ex);
-            throw new ConnectTimeoutException("connection to " + uri + " timed out");
+        } catch(IOException ex) {
+            logger.warn(ex);
+            throw new ConnectTimeoutException(String.format("%s-request to %s timed out", request.getConfig(), request.getURI()));
         } finally {
-            get.releaseConnection();
+            request.releaseConnection();
         }
     }
 }

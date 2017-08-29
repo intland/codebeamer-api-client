31c191fe64b678fcd5fe5b3fc8d628e41e3fb9c0
Martin Gansler <martin.gansler@intland.com>
2017-08-29 08:31:46 +0200

    add main() method

---
+++ src/main/java/com/codebeamer/api/client/Client.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2017 Intland Software (support@intland.com)
+ *
+ */
+package com.codebeamer.api.client;
+
+import org.apache.commons.cli.*;
+import org.apache.log4j.Logger;
+
+import java.nio.file.Paths;
+
+public class Client {
+    private static Logger logger = Logger.getLogger(Client.class);
+
+    public static void main(String[] args) throws Exception {
+        Options options = new Options();
+        Option pathOption = new Option("p", "path", true, "path of XUnit results");
+        pathOption.setRequired(true);
+        options.addOption(pathOption);
+
+        CommandLineParser parser = new DefaultParser();
+        HelpFormatter formatter = new HelpFormatter();
+        CommandLine cmd;
+
+        try {
+            cmd = parser.parse(options, args);
+        } catch (ParseException ex) {
+            logger.warn(ex.getMessage());
+            formatter.printHelp("codebeamer-api-client", options);
+
+            System.exit(1);
+            return;
+        }
+
+        String inputPath = cmd.getOptionValue("path");
+
+        XUnitFileCollector collector = new XUnitFileCollector();
+        collector.listFiles(collector.getListOfFilesForDirectory(Paths.get(inputPath).toFile()));
+    }
+}

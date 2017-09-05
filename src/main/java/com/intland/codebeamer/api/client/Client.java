4af58275f7a9e68e37ee29fa05259853a30b3209
Martin Gansler <martin.gansler@intland.com>
2017-09-05 13:59:23 +0200

    get CB version and list files

--- src/main/java/com/intland/codebeamer/api/client/Client.java
+++ src/main/java/com/intland/codebeamer/api/client/Client.java
@@ -4,6 +4,8 @@
  */
 package com.intland.codebeamer.api.client;
 
+import com.intland.codebeamer.api.client.rest.RestAdapter;
+import com.intland.codebeamer.api.client.rest.RestAdapterImpl;
 import org.apache.commons.cli.*;
 import org.apache.log4j.Logger;
 
@@ -17,9 +19,22 @@ public class Client {
 
     public static void main(String[] args) throws Exception {
         Options options = new Options();
+
         Option pathOption = new Option("p", "path", true, "path of XUnit results");
         pathOption.setRequired(true);
         options.addOption(pathOption);
+
+        Option cbAddressOption = new Option("u", "url", true, "url of codebeamer");
+        cbAddressOption.setRequired(true);
+        options.addOption(cbAddressOption);
+
+        Option usernameOption = new Option("U", "username", true, "username for codebeamer");
+        usernameOption.setRequired(true);
+        options.addOption(usernameOption);
+
+        Option passwordOption = new Option("P", "password", true, "password for codebeamer");
+        passwordOption.setRequired(true);
+        options.addOption(passwordOption);
 
         CommandLineParser parser = new DefaultParser();
         HelpFormatter formatter = new HelpFormatter();
@@ -35,7 +50,15 @@ public class Client {
             return;
         }
 
-        String inputPath = cmd.getOptionValue("path");
+        final String inputPath = cmd.getOptionValue("path");
+        final String url = cmd.getOptionValue("url");
+        final String username = cmd.getOptionValue("username");
+        final String password = cmd.getOptionValue("password");
+
+        Configuration config = new Configuration(url, username, password);
+        RestAdapter rest = new RestAdapterImpl(config, null);
+
+        logger.info(String.format("Target Codebeamer Version is %s", rest.getVersion()));
 
         XUnitFileCollector collector = new XUnitFileCollector();
         collector.listFiles(collector.getListOfFilesForDirectory(Paths.get(inputPath).toFile()));
